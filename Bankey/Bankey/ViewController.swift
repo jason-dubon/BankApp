801a963267be5fbb551fe5ef068523de8af081b9
Jason Dubon <jasondubon@Jasons-MBP.attlocal.net>
2022-05-26 15:41:06 -0400

    new project:

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Jason Dubon on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
